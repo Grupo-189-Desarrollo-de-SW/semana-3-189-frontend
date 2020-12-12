55fb2bb3a57fa5b9323bec5a79d7ceaa3788a371
Gustavo García <gustavogarcia@utp.edu.co>
2020-12-11 23:34:16 -0500

    Vistas de login y home completadas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
 
+const base= axios.create({
+  baseURL: "http://localhost:3000"
+  // baseURL:"https://safe-waters-18200.herokuapp.com"
+});
+
+Vue.prototype.$http= base;
 Vue.config.productionTip = false
 
 new Vue({
