78b694460c07f0ca6301dddb9d9d22aba7fcb40a
Gustavo García <gustavogarcia@utp.edu.co>
2020-12-19 16:42:22 -0500

    Vista de signup totalmente funcional

--- src/components/TheSignup.vue
+++ src/components/TheSignup.vue
@@ -7,13 +7,77 @@
       ></div>
 
       <div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
-        <form class="login100-form validate-form">
+        <form class="login100-form validate-form" @submit.prevent="signup()">
           <span class="login100-form-title p-b-59">
             Regístrate
           </span>
 
-          <div v-for="item in inputs" :key="item.id" @submit.prevent="submit">
-            <input-signup :info="item"></input-signup>
+          <div
+            class="wrap-input100 validate-input"
+            data-validate="Nombre es obligatorio"
+            :class="validateAlertNombre"
+          >
+            <span class="label-input100">Nombre</span>
+            <input
+              class="input100"
+              v-model="usuario.name"
+              type="text"
+              name="Nombre"
+              placeholder="Nombre..."
+              :class="hasvalueNombre"
+              @blur="hasvalueNombre = validateNombre()"
+              @focus="hasvalueNombre = 'init'"
+            />
+            <span class="focus-input100"></span>
+          </div>
+
+          <div
+            class="wrap-input100 validate-input"
+            data-validate="ex@abc.xyz"
+            :class="validateAlertEmail"
+          >
+            <span class="label-input100">Email</span>
+            <input
+              class="input100"
+              v-model="usuario.email"
+              type="text"
+              name="Email"
+              placeholder="Correo electrónico..."
+              :class="hasvalueEmail"
+              @blur="hasvalueEmail = validateEmail()"
+              @focus="hasvalueEmail = 'init'"
+            />
+            <span class="focus-input100"></span>
+          </div>
+
+          <div class="wrap-input100">
+            <span class="label-input100">Rol</span>
+            <select class="input100" name="Rol" v-model="usuario.rol">
+              <option value=""></option>
+              <option value="Administrador">Administrador</option>
+              <option value="Vendedor">Vendedor</option>
+              <option value="Almacenero">Almacenero</option>
+            </select>
+            <span class="focus-input100"></span>
+          </div>
+
+          <div
+            class="wrap-input100 validate-input"
+            data-validate="Al menos 8 caracteres"
+            :class="validateAlertPassword"
+          >
+            <span class="label-input100">Contraseña</span>
+            <input
+              class="input100"
+              v-model="usuario.password"
+              type="password"
+              name="Contraseña"
+              placeholder="Contraseña..."
+              :class="hasvaluePassword"
+              @blur="hasvaluePassword = validatePassword()"
+              @focus="hasvaluePassword = 'init'"
+            />
+            <span class="focus-input100"></span>
           </div>
 
           <div class="flex-m w-full p-b-33">
@@ -58,44 +122,93 @@
 </template>
 
 <script>
-import InputSignup from "./InputSignup.vue";
+import swal from "sweetalert";
 export default {
-  components: { InputSignup },
   name: "TheSignup",
   data() {
     return {
       img: require("../assets/images/bg-02.jpg"),
-      check:false,
-      inputs: [
-        {
-          type: "text",
-          name: "Nombre",
-          placeholder: "Nombre...",
-          dataValidate: "Nombre es obligatorio",
-        },
-        {
-          type: "text",
-          name: "Email",
-          placeholder: "Correo electrónico...",
-          dataValidate: "Email es obligatorio: ex@abc.xyz",
-        },
-        {
-          type: "text",
-          name: "Rol",
-          placeholder: "Rol...",
-          dataValidate: "Rol es obligatorio",
-        },
-        {
-          type: "password",
-          name: "Contraseña",
-          placeholder: "Contraseña...",
-          dataValidate: "Al menos 8 caracteres",
-        },
-      ],
+      usuario: {
+        name: "",
+        email: "",
+        password: "",
+        rol: "",
+      },
+      check: false,
+      hasvalueNombre: "init",
+      hasvaluePassword: "init",
+      hasvalueEmail: "init",
     };
   },
   methods: {
+    validateInput: function(hasvalue) {
+      if (hasvalue === "init") {
+        return "";
+      } else if (hasvalue === "has-value") {
+        return "true-validate";
+      } else {
+        return "alert-validate";
+      }
+    },
 
+    validateEmail: function() {
+      let val =
+        this.usuario.email
+          .trim()
+          .match(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+          ) !== null;
+      return this.hasvalue(val);
+    },
+
+    validatePassword: function() {
+      let val = this.usuario.password.length >= 8;
+      return this.hasvalue(val);
+    },
+
+    validateNombre: function() {
+      let val = this.usuario.name.trim() !== "";
+      return this.hasvalue(val);
+    },
+
+    hasvalue: function(validation) {
+      return validation ? "has-value" : "";
+    },
+    signup: async function() {
+      this.hasvalueNombre = this.validateNombre();
+      this.hasvalueEmail = this.validateEmail();
+      this.hasvaluePassword = this.validatePassword();
+
+      if (this.hasvalueNombre && this.hasvalueEmail && this.hasvaluePassword) {
+        if (this.check) {
+          try {
+            let response = await this.$http.post(
+              "/api/user/signup",
+              this.usuario
+            );
+            swal("Exitoso", "Se ha registrado exitosamente", "success");
+            this.$router.push("/");
+          } catch (error) {}
+        } else {
+          swal("Error", "No has aceptado los terminos de uso", "error");
+        }
+      } else {
+        swal("Error", "Datos incorrectos", "error");
+      }
+    },
+  },
+  computed: {
+    validateAlertNombre: function() {
+      return this.validateInput(this.hasvalueNombre);
+    },
+
+    validateAlertPassword: function() {
+      return this.validateInput(this.hasvaluePassword);
+    },
+
+    validateAlertEmail: function() {
+      return this.validateInput(this.hasvalueEmail);
+    },
   },
 };
 </script>
